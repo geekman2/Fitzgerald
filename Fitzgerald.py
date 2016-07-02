deed82365faf4f50513eac8c01976175ed39e53a
geekman2 <geekmann2@gmail.com>
2016-07-01 23:06:13 -0700

    Individual route plotting working, moving on to master route generation

--- Fitzgerald.py
+++ Fitzgerald.py
@@ -3,15 +3,16 @@ import itertools
 import pandas as pd
 from matplotlib import pyplot as plt
 
-locations = ["1234 Alpha Lane"," 456 Bravo Street"," 2345 Charlie Avenue",
-             "4567 Delta Place","795 Echo Drive"] #Sample Locations
+locations = ["1234 Alpha Lane", " 456 Bravo Street", " 2345 Charlie Avenue",
+             "4567 Delta Place", "795 Echo Drive"]  # Sample Locations
 
-people = ["Alfred","Becka","Chad","Dale","Emily"] #Sample People
+people = ["Alfred", "Becka", "Chad", "Dale", "Emily"]  # Sample People
 time_frames = {}
 stops = []
 
+#TODO Refactor out test code
 
-def get_time_between(locA,locB):
+def get_time_between(locA, locB):
     """
     :param locA:First Location:String
     :param locB: Second Location:String
@@ -22,54 +23,67 @@ def get_time_between(locA,locB):
     Eventual Purpose: Query an API for time interval values
     """
 
-    location_pair = "<>".join(sorted([locA,locB])) #Sort the pair so that A>B doesn't get duplicated with B>A
-    if location_pair in time_frames: #Make sure we haven't done this before
+    location_pair = "<>".join(sorted([locA, locB]))  # Sort the pair so that A>B doesn't get duplicated with B>A
+    if location_pair in time_frames:  # Make sure we haven't done this before
         return time_frames[location_pair]
 
     else:
-        if location_pair[0] == location_pair[1]: #If the arrival point is the same as the destination, return time = 0
+        if location_pair[0] == location_pair[1]:  # If the arrival point is the same as the destination, return time = 0
             time_frames[location_pair] = 0
             return 0
         else:
-            move_time = random.randint(0,30) #otherwise, return a random time
+            move_time = random.randint(0, 30)  # otherwise, return a random time
             time_frames[location_pair] = move_time
             return move_time
 
-for person in people:
-    current_time = 0
-    places = [(lambda x:random.choice(locations))(x) for x in range(3)]
-    for ii,place in enumerate(places):
-        arrive_at = current_time+random.randint(1,5)
-        leave_after = arrive_at+random.randint(1,5)
-        current_time = leave_after
-        if ii == 0:
-            print arrive_at,leave_after
-            home = random.choice(locations)
-            stops.append(
-                {"Name":person,
-                 "from":home,
-                 "to":places[ii],
-                 "due":arrive_at,
-                 "leave":leave_after,
-                 }
-            )
-        else:
-            stops.append(
-                {"Name":person,
-                 "from":home,
-                 "to":places[ii],
-                 "due":arrive_at,
-                 "leave":leave_after,
-                 }
-            )
 
-stops_df = pd.DataFrame(stops)
+def generate_test_cases():
+    for person in people:
+        current_time = 0
+        place_indexes = [(lambda x: random.randint(0, len(locations) - 1))(x) for x in range(3)]
+        for ii, place in enumerate(place_indexes):
+            arrive_at = current_time + random.randint(1, 5)
+            leave_after = arrive_at + random.randint(1, 5)
+            current_time = leave_after
+            if ii == 0:
+                home = random.randint(0, len(locations) - 1)
+                stops.append(
+                    {"Name": person,
+                     "from": home,
+                     "to": place_indexes[ii],
+                     "due": arrive_at,
+                     "leave": leave_after,
+                     }
+                )
+            else:
+                stops.append(
+                    {"Name": person,
+                     "from": place_indexes[ii - 1],
+                     "to": place_indexes[ii],
+                     "due": arrive_at,
+                     "leave": leave_after,
+                     }
+                )
 
-print stops_df
+stops_dataframe = pd.DataFrame(stops)
 
-D = {"a":10,"b":2}
 
-plt.plot(range(len(D)), D.values(), align='center')
-plt.xticks(range(len(D)), D.keys())
+def plot_route():
+    for pers in people:
+        personal_stops = stops_dataframe.loc[stops_dataframe["Name"] == pers]
+        times = sorted(list(personal_stops["due"]) + list(personal_stops["leave"]))
+        places = [val for val in personal_stops["from"] for _ in (0, 1)]
+        plt.plot(times, places, linewidth=10)
 
-plt.show()
+    plt.xticks(range(0, 40))
+    plt.yticks(range(len(locations)), locations)
+    plt.margins(0.1, axis="y")
+    plt.tight_layout()
+
+    plt.show()
+
+
+def route_builder():
+    order = random.shuffle(list[stops_dataframe["from"]])
+
+
